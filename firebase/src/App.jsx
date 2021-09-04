7f1f9501b928481f8191fa032e89912c97eb2ef5
SantiDani <profsantiRP@gmail.com>
2021-09-04 17:59:32 +0200

    Add Router

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -1,10 +1,33 @@
 
 import './App.css';
 
+import {
+  BrowserRouter as Router, // Es el tag que engloba todas la rutas
+  Switch, // Fo change between pages
+  Route, // It is a router. It is necessary for one for page
+} from "react-router-dom";
+
+import Home from './components/Home';
+import Admin from './components/Admin';
+import Login from './components/Login';
+
 function App() {
   return (
     <div className="App">
       <h2>Hello World</h2>
+      <Router>
+        <Switch>
+          <Route path="/" >
+            <Home></Home>
+          </Route>
+          <Route path="/admin">
+            <Admin/>
+          </Route>
+          <Route path="/login" >
+            <Login/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ firebase/src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <div>
+            <h2>Component Login</h2>
+        </div>
+    );
+};
+
+export default Menu;
