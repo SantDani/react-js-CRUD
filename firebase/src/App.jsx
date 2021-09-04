6b7ff2cea1c76a087036f844f42d5b68f0b4ccf0
SantiDani <profsantiRP@gmail.com>
2021-09-04 18:06:20 +0200

    add navBar

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -5,17 +5,21 @@ import {
   BrowserRouter as Router, // Es el tag que engloba todas la rutas
   Switch, // Fo change between pages
   Route, // It is a router. It is necessary for one for page
+  Link
 } from "react-router-dom";
 
 import Home from './components/Home';
 import Admin from './components/Admin';
 import Login from './components/Login';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World</h2>
+      
+      
       <Router>
+        <Menu/>
         <Switch>
           <Route path="/" >
             <Home></Home>
--- firebase/src/components/Menu.jsx
+++ firebase/src/components/Menu.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
     return (
         <div>
-            <h2>Component Login</h2>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">Login</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/admin">Admin</Link>
+                    </li>
+                    
+                </ul>
+            </nav>
         </div>
     );
 };
