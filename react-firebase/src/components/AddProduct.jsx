09bf57098345b2f716861d6e7e041fec1e551176
SantiDani <profsantiRP@gmail.com>
2021-09-05 21:39:53 +0200

    Add list Recent

--- react-firebase/src/components/AddProduct.jsx
+++ react-firebase/src/components/AddProduct.jsx
@@ -1,25 +1,69 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import {firestoreDB} from './../firebaseConfig'
+
+import { collection, addDoc } from "firebase/firestore";  // TODO Remove
+import {  getDocs } from "firebase/firestore";  // TODO Remove
+
 
 const AddProduct = () => {
 
+    
     const[nameProduct, setNameProduct] = useState('')
     const[priceProduct, setPriceProduct] = useState('')
     const[msgError, setMsgError] = useState('')
 
-    
-    const newProduct = (e) => {
-        e.preventDefault();
+    const[listProducts, setListProducts] = useState([])
 
+    const getProducts = async () => {
+        const querySnapshot = await getDocs(collection(firestoreDB, "product"));
+
+        let auxProducts = [];
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data().name} , ${doc.data().price}`);
+            auxProducts.push({id: doc.id, name: doc.data().name, price: doc.data().price})
+            
+        })
+
+        setListProducts(auxProducts)
+        console.log('log - auxProducts', auxProducts);
+    }
+
+
+    useEffect(() => {
+        
+        getProducts();
+    }, [])
+    
+    const newProduct = async (e) => {
+        e.preventDefault();
+        console.log('log -', nameProduct, priceProduct);
         if(!nameProduct.trim()) {
-            setMsgError('It is necessary to put the name of product')
+            setMsgError('It is necessary to put the NAME of product')
             return;
         }
 
         if(!priceProduct.trim()) {
-            setMsgError('It is necessary to put the Price of product')
+            setMsgError('It is necessary to put the PRICE of product')
             return;
         }
 
+        setMsgError('')
+
+        try {
+            
+            const docRef = await addDoc(collection(firestoreDB, "product"), {
+                "name" : nameProduct,
+                "price" : priceProduct,
+            })
+
+            console.log('Document Write with ID ', docRef.id);
+
+            getProducts();
+        } catch (error) {
+            console.error(e);
+            
+        }
+        
     }
 
     return (
@@ -32,21 +76,47 @@ const AddProduct = () => {
             <div className="col m-2">
                 <h3>New product</h3>
                 <form 
-                    onClick={(event) => {newProduct(event)}}
+                    onSubmit={(event) => {newProduct(event)}}
                     className="form-group"
                     >
-                    <input type="text" className="form-control" placeholder="Name Product"/>
-                    <input type="number"  className="form-control mt-2" placeholder="Price product. Ej. 51"/>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="Name Product"
+                        onChange={(event => {setNameProduct(event.target.value)})}/>
+                    <input 
+                        type="number"  
+                        className="form-control mt-2" 
+                        placeholder="Price product. Ej. 51"
+                        onChange={(event => {setPriceProduct(event.target.value)})}/>
                     <button
-                        className="btn btn-dark btn-block form-control mt-4">
+                        className="btn btn-dark btn-block form-control mt-4"
+                        type="submit">
                         Add Product
+                    
                     </button>
+                    {
+                        msgError && <label className='form-control alert alert-danger mt-2'>{msgError}</label>
+                    }
                 </form>
             </div>
             <div className="col m-2">
+
                 <h3>List Recent</h3>
+                <ul className="list-group list-group-flush">
+                    {
+                        listProducts?.map(product => 
+                            <li key={product.id} className="list-group-item list-group-item-action">
+                                {product.name}  - {product.price} €
+                                <div className="d-flex justify-content-around">
+                                    <button className="btn btn-primary">Edit</button>
+                                    <button className="btn btn-danger ">Remove</button>
+                                </div>
+                            </li>)
+                    }
+                </ul>
             </div>
-            {/* <div className="col "></div> */}
+            
             
         </div>
     );
--- react-firebase/src/firebaseConfig.js
+++ react-firebase/src/firebaseConfig.js
@@ -4,12 +4,16 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 //https://firebase.google.com/docs/web/setup#available-services
+// Firebase - Auth
 import { 
   getAuth, 
   createUserWithEmailAndPassword, 
   signInWithEmailAndPassword,
   onAuthStateChanged
 } from "firebase/auth";
+
+// Firebase - Firestore
+import { getFirestore } from "firebase/firestore";
 
 // Import SDK from file
 import {firebaseConfig} from './firebaseConfig.json'
@@ -19,11 +23,14 @@ import {firebaseConfig} from './firebaseConfig.json'
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+const firestoreDB = getFirestore();
+
 export {
   app,
   analytics,
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  onAuthStateChanged
+  onAuthStateChanged, 
+  firestoreDB
  }
